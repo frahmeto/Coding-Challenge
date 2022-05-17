2d176ec629accaa098ee0a80cfe08f13b093bef5
frahmeto <66395625+frahmeto@users.noreply.github.com>
2022-05-17 07:26:56 -0500

    Create index.js

---
+++ week5/index.js
@@ -0,0 +1,16 @@
+function addUp(num) {
+		let sum = 0;
+  for(let i =0; i <= num; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+
+
+
+
+
+//Test Passed: Value == 10
+//Test Passed: Value == 91
+//Test Passed: Value == 180300
