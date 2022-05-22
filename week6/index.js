7ee612d66c7c6e36bbea56014374b1b705bc770b
frahmeto <66395625+frahmeto@users.noreply.github.com>
2022-05-22 14:09:46 -0500

    Update index.js

--- week6/index.js
+++ week6/index.js
@@ -5,8 +5,6 @@
 	// The crowd of letters is all in uppercase.
 	// Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
 
-
-
 function detectWord(str) {
 	let x = "";
 	for(let i =0; i < str.length; i++){
